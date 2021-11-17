0224879c92e5d76fb194d305130ad2b641b44056
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-17 15:28:54 +0000

    Add tests tto DockingStation spec to get bike and check if working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,19 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do 
-it 'DockingStation responds to release_bike' do
+    it 'DockingStation responds to release_bike' do
         docking_station = DockingStation.new
         expect(docking_station).to respond_to(:release_bike)
+    end
+
+    it 'gets a bike from the bike class' do
+        bmx = Bike.new
+        expect(bmx).to be_an_instance_of(Bike)
+    end
+
+    it 'tests if the bike is working' do
+        bmx = Bike.new
+        expect(bmx.working?).to be true
     end
 end
