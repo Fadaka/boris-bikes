9122ac30720758b0e21fe3d40b7e76b0e394e02d
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-17 15:12:35 +0000

    IPass test for Bike instances respond_to working?

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,3 +1,5 @@
 class Bike
-
+  def working?
+    'i am working'
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,8 @@
 require 'bike'
 
 describe Bike do
-  
+  it 'tests if bike is working' do
+    bmx = Bike.new
+    expect(bmx).to respond_to(:working?)
+  end
 end
