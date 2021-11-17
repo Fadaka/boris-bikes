5a6adaf02464ce3cd371d38b3873694b467331d4
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-17 15:05:57 +0000

    Add Bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do 
-    it 'DockingStation responds to release_bike' do
+it 'DockingStation responds to release_bike' do
         docking_station = DockingStation.new
         expect(docking_station).to respond_to(:release_bike)
     end
