8c9cf6714c2ec70102becf4410381ba0b1767c1e
Fadaka <master_kunle14@hotmail.co.uk>
2021-11-21 12:37:03 +0000

    refactored code using constants, step-16

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,12 @@
-class DockingStation
+class DockingStation ()
     attr_reader :docked_bikes
+    DEFAULT_CAPACITY = 20
     def initialize
         @docked_bikes = []
     end
 
     def release_bike
-        if @docked_bikes.empty?
+        if empty?
             raise "Dock is empty, no bikes to release"
         else 
             Bike.new
@@ -13,11 +14,24 @@ class DockingStation
     end
     
     def dock(bike)
-        if @docked_bikes.length == 1
+        if full?  
             raise "Dock is currently full"
         end
         @docked_bikes.push(bike)
         p @docked_bikes
         return true
+    end
+
+  private
+    def full?
+        if @docked_bikes.length >= DEFAULT_CAPACITY
+            return true
+        end
+    end
+
+    def empty?
+       if @docked_bikes.empty?
+            return true
+       end
     end
 end
